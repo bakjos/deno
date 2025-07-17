2f91cde8f9360f333c8700e4bea1367aefb957ed
Gio Gutierrez <giovanny.gutierrez@commure.com>
2025-07-16 22:34:44 -0500

    feat(deno-config): Move workspace inner folder

--- ext/tls/lib.rs
+++ ext/tls/lib.rs
@@ -190,7 +190,6 @@ impl ServerCertVerifier for NoServerNameVerification {
         if matches!(
           cert_error,
           rustls::CertificateError::NotValidForName
-            | rustls::CertificateError::NotValidForNameContext { .. }
         ) {
           Ok(ServerCertVerified::assertion())
         } else {
